6bf5d0dadd061c5fcd4ed4cbca4475e03026d924
Dailey <hannah.dailey@waterboards.ca.gov>
2023-04-18 11:33:00 -0700

    day 3

--- 2023/scripts/03_faunaflora.R
+++ 2023/scripts/03_faunaflora.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(showtext)
 library(rnaturalearth)
+library(ggtext)
 
-font_add_google("Roboto")
+font_add_google("Cabin")
+font_add_google("Cabin Condensed")
 showtext_auto()
 
 # Tidy Tuesday 2023, Week 16 Data - https://github.com/rfordatascience/tidytuesday/blob/master/data/2023/2023-04-18/readme.md
@@ -15,15 +17,25 @@ crops <- tuesData$founder_crops %>%
   slice_max(prop, n = 1) %>%
   ungroup()
 
-map <- ne_countries(returnclass = "sf")
+map <- ne_countries(continent = c("africa", "asia"), returnclass = "sf")
 
 plot_03 <- ggplot() +
-  geom_sf(data = map) +
-  geom_rect(data = data.frame(),
-            mapping = aes(xmin = 30.0, xmax = 48.6,
-                          ymin = 29.8, ymax = 38.5)) +
+  geom_sf(data = map, colour = "black", fill = "grey95") +
   geom_point(data = crops,
-             mapping = aes(x  = longitude, y = latitude, colour = category))
-
-
+             mapping = aes(x  = longitude, y = latitude, shape = category,
+                           colour = category)) +
+  labs(x = "",
+       y = "",
+       title = "Neolithic Founder Crops",
+       subtitle = "Eight **founder crops** — emmer wheat, einkorn wheat, barley, lentil, pea, chickpea, bitter vetch, and flax — have long been thought to have been the bedrock of Neolithic economies. <br>
+       The map below shows the site locations noted within the Origins of Agriculture database.",
+       caption = "Source: Orgins of Agriculture | #30DayChartChallenge | Day 3 | Fauna Flora") +
+  theme_void() +
+  theme(text = element_text(size = 24, family = "Cabin Condensed"),
+        plot.title = element_text(family = "Cabin", face = "bold",
+                                  margin = margin(b = 10)),
+        plot.subtitle = element_textbox_simple(lineheight = 0.5, hjust = 0, margin = margin(b = 10)),
+        legend.position = "top",
+        legend.title = element_blank())
   
+        
