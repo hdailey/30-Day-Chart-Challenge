e5aad93a7cc14449cdf45f08c0ea81411d8a67d3
Hannah Dailey <91282117+hdailey@users.noreply.github.com>
2023-04-19 12:57:24 -0700

    Update 04_historical.R

--- 2023/scripts/04_historical.R
+++ 2023/scripts/04_historical.R
@@ -5,6 +5,7 @@ font_add_google("Cabin")
 font_add_google("Cabin Condensed")
 showtext_auto()
 
+# Data is from #TidyTuesday 2020 - June 16, 2020: https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-06-16/
 tuesdata <- tidytuesdayR::tt_load(2020, week = 25)
 
 routes <- tuesdata$slave_routes
