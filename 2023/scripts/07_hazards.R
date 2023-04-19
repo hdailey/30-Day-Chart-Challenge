3ee9ae7d63cc575253e3dd3755c96732fe06e88c
Dailey <hannah.dailey@waterboards.ca.gov>
2023-04-19 08:55:43 -0700

    script

--- 2023/scripts/07_hazards.R
+++ 2023/scripts/07_hazards.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(showtext)
 library(ggridges)
 library(MetBrewer)
+library(lubridate)
 
 font_add_google("Cabin")
 font_add_google("Cabin Condensed")
@@ -10,4 +11,8 @@ showtext_auto()
 # Manually download data source from USGS:
 # Avalanche occurrence records along the Going-to-the-Sun Road, Glacier National Park, Montana Data: https://doi.org/10.5066/P9BO1LHQ
 
-avyData <- read_csv(here::here("2023/data/07_hazards.csv")
+avyData <- read_csv(here::here("2023/data/07_hazards.csv")) %>%
+  mutate(Date = mdy(Date),
+         Month = month(Date),
+         Year = year(Date)) %>%
+  select(c(ID, Date, Year, Month, AvalancheType))
