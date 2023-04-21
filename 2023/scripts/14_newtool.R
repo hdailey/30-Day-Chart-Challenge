543466c8426b19f8a50ab538361cee227ec92aa1
hdailey <hamdailey@gmail.com>
2023-04-20 21:04:03 -0700

    update

--- 2023/scripts/14_newtool.R
+++ 2023/scripts/14_newtool.R
@@ -1,1 +1,11 @@
 library(tidyverse)
+library(ggstraw)
+
+# Data is from Tidy Tuesday, 2021 Week 31: https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-07-27/
+
+olyData <- tidytuesdayR::tt_load(2021, week = 31)
+
+olympics <- olyData$olympics %>%
+  filter(noc == "USA") %>%
+  na.omit()
+
