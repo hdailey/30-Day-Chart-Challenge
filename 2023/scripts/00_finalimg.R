386944b61a2c94142d0447078212c98005e34b54
Dailey <Hannah.Dailey@Waterboards.ca.gov>
2023-04-26 15:11:16 -0700

    update

---
+++ 2023/scripts/00_finalimg.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(magick)
+
+img_list <- list.files(path = here::here("2023/charts/"), full.names = TRUE)
+
+img_montage <- image_read(img_list) %>% 
+  image_montage(tile = '5x4', 
+                geometry = "x1000+0+0", 
+                shadow = FALSE, 
+                bg = "grey95")
+
+image_write(img_montage, format = "png", 
+            path = here::here("2023/charts/00_finalimg.png"), 
+            quality = 100)
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 A repository containing code for the #30DayChartChallenge. Check out the challenge at [twitter.com/30DayChartChall](https://twitter.com/30DayChartChall)
 
 ## 2023
+<p align="center">
+<img src="2023/charts/00_finalimg.png?raw=true" width="100%">
+</p>
